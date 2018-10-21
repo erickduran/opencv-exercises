804705f83350fe9a99dad9fecf31c1e62e5c66f2
Kevin Madrid <kmadridr@Kevins-MacBook-Pro.local>
2018-10-20 23:29:57 -0700

    + Console Input added

---
+++ ex1/main.py
@@ -0,0 +1,17 @@
+import cv2
+import numpy as np
+
+img = cv2.imread('lena.png')
+
+size = int(input("Ingrese la variable d:"))
+
+# generating the kernel
+kernel_motion_blur = np.zeros((size, size))
+kernel_motion_blur[int((size-1)/2), :] = np.ones(size)
+kernel_motion_blur = kernel_motion_blur / size
+
+# applying the kernel to the input image
+output = cv2.filter2D(img, -1, kernel_motion_blur)
+
+cv2.imshow('Motion Blur', output)
+cv2.waitKey(0)
